6d2926b4d6ea55ec4e836d7915f1a19fa45d031b
Gergely Fábián <gergo.fb@gmail.com>
2021-09-23 13:11:58 +0200

    Reproducing rules_scala issue #1291
    https://github.com/bazelbuild/rules_scala/issues/1291

--- example-maven/src/main/scala/mypackage/Maven.scala
+++ example-maven/src/main/scala/mypackage/Maven.scala
@@ -5,3 +5,15 @@ import com.twitter.algebird.Semigroup
 object Maven {
   val message = Semigroup.plus("hello ", "world")
 }
+
+object RulesScalaIssue1291Repro {
+  // This is the method that is reproducing https://github.com/bazelbuild/rules_scala/issues/1291.
+  // If you comment the method's implementation and replace it with `true`, no error will thrown.
+  // Run `bazel coverage //...` to reproduce the issue.
+  def getMyValue(literal: String): Boolean =
+    literal match {
+      case s if s == "true" => true
+      case s if s == "false" => false
+      case _ => throw new Exception("Boooom!")
+    }
+}
